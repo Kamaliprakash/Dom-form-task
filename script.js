1e0bcde8f1d541b80ba2852260f739329c6868c3
Kamaliprakash <kamaliprakash0307@gmail.com>
2023-12-01 00:20:02 +0530

    DOM task form

---
+++ script.js
@@ -0,0 +1,11 @@
+const txt1 = document.getElementById("fname");
+const txt2 = document.getElementById("lname");
+const txt3 = document.getElementById("add");
+const txt4 = document.getElementById("pcode");
+const txt5 = document.getElementById("common");
+const txt6 = document.getElementById("food");
+const txt7 = document.getElementById("state");
+const txt8 = document.getElementById("country");
+
+const btn1 = document.getElementById("btn1");
+let out1 = document.getElementById()
