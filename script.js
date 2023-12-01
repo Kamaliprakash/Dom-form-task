7dd75952efae9f510d4bc2702426eeae803b4a13
Kamaliprakash <kamaliprakash0307@gmail.com>
2023-12-01 12:51:53 +0530

    changed

--- script.js
+++ script.js
@@ -28,57 +28,16 @@ function addData() {
     cell6.innerHTML = food;
     cell7.innerHTML = state;
     cell8.innerHTML = country;
+ 
+
+
 
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  const txt1 = document.getElementById("form");
-//  const btn1 = document.getElementById("btn1");
-//  const  out1 = document.getElementById("output1");
-
 //  function fun1(){
-//     out1.innerHTML= txt1.value;
-//  }
+//      out1.innerHTML= txt1.value;
+//   }
 
-//  btn1.addEventListener('click',fun1);
+//   btn1.addEventListener('click',fun1);
